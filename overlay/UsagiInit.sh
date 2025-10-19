00cbc909d47f5b588ba0eb84f48ed0ee59369058
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2025-10-19 04:12:06 +0000

    FEAT: export http proxy for cloudflared

--- overlay/UsagiInit.sh
+++ overlay/UsagiInit.sh
@@ -1,5 +1,7 @@
-/usr/sbin/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 > /dev/null &
-sleep 10s
+#!/UsagiInit
+/usr/sbin/tailscaled -no-logs-no-support --tun=userspace-networking --socks5-server=localhost:1055 > /dev/null &
+sleep 3s
 /usr/bin/tailscale up --auth-key $TS_AUTHKEY
-export ALL_PROXY=socks5://localhost:1055
-/usr/local/bin/cloudflared tunnel --no-autoupdate run --token $CLOUDFLARE_TOKEN > /dev/null &
+export HTTP_PROXY=socks5://127.0.0.1:1055
+export HTTPS_PROXY=socks5://127.0.0.1:1055
+/usr/local/bin/cloudflared tunnel --no-autoupdate --loglevel error --token $CLOUDFLARE_TOKEN > /dev/null &
