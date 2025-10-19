7f4f7dc661318a59053cf1004f373eb070973d0d
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2025-10-19 02:44:14 +0000

    FIX: ts_authkey flag

--- overlay/UsagiInit.sh
+++ overlay/UsagiInit.sh
@@ -1,5 +1,5 @@
 /usr/sbin/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 > /dev/null &
 sleep 10s
-/usr/bin/tailscale up --auth-key=$TS_AUTHKEY
+/usr/bin/tailscale up --auth-key $TS_AUTHKEY
 export ALL_PROXY=socks5://localhost:1055
 /usr/local/bin/cloudflared tunnel --no-autoupdate run --token $CLOUDFLARE_TOKEN > /dev/null &
