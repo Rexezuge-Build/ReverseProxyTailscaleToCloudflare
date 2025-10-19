6051512a8a15dfd93971071dbfb76f7774ef5b50
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2025-10-19 02:27:21 +0000

    FEAT: start cloudflared as service

--- overlay/UsagiInit.sh
+++ overlay/UsagiInit.sh
@@ -2,3 +2,4 @@
 sleep 10s
 /usr/bin/tailscale up --auth-key=$TS_AUTHKEY
 export ALL_PROXY=socks5://localhost:1055
+/usr/local/bin/cloudflared tunnel --no-autoupdate run --token $CLOUDFLARE_TOKEN > /dev/null &
