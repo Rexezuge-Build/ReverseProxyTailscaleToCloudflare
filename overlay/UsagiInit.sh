04058591ef7ae07fd430be340d15a896a3832356
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2025-12-28 09:02:28 -0600

    FEAT: set device name

--- overlay/UsagiInit.sh
+++ overlay/UsagiInit.sh
@@ -1,7 +1,7 @@
 #!/UsagiInit
 /usr/sbin/tailscaled -no-logs-no-support --tun=userspace-networking --socks5-server=localhost:1055 > /dev/null 2> /dev/null &
 sleep 10s
-/usr/bin/tailscale up --auth-key $TS_AUTHKEY --accept-dns=false --accept-routes=false
+/usr/bin/tailscale up --auth-key $TS_AUTHKEY --accept-dns=false --accept-routes=false --hostname="z99-reverse-proxy-to-cloudflare"
 export HTTP_PROXY=socks5://127.0.0.1:1055
 export HTTPS_PROXY=socks5://127.0.0.1:1055
 /usr/local/bin/cloudflared tunnel --no-autoupdate --loglevel error run --token $CLOUDFLARE_TOKEN > /dev/null 2> /dev/null &
