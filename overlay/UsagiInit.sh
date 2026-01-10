328a54624a6945d369b876b72531d3f39eb94c26
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2026-01-09 20:28:58 -0600

    FIX: enable tailscale and cloudflared logs

--- overlay/UsagiInit.sh
+++ overlay/UsagiInit.sh
@@ -1,8 +1,8 @@
 #!/UsagiInit
 /HealthCheck-Go > /dev/null 2> /dev/null &
-/usr/sbin/tailscaled -no-logs-no-support --tun=userspace-networking --socks5-server=localhost:1055 > /dev/null 2> /dev/null &
+/usr/sbin/tailscaled -no-logs-no-support --tun=userspace-networking --socks5-server=localhost:1055
 sleep 10s
 /usr/bin/tailscale up --auth-key $TS_AUTHKEY --accept-dns=false --accept-routes=false
 export HTTP_PROXY=socks5://127.0.0.1:1055
 export HTTPS_PROXY=socks5://127.0.0.1:1055
-/usr/local/bin/cloudflared tunnel --no-autoupdate --loglevel error run --token $CLOUDFLARE_TOKEN > /dev/null 2> /dev/null &
+/usr/local/bin/cloudflared tunnel --no-autoupdate --loglevel error run --token $CLOUDFLARE_TOKEN
